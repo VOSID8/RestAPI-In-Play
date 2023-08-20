8b01b142433ebbecac3d377f31fabdd32e32c82a
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2023-08-21 02:37:05 +0530

    Added updates

---
+++ app/controllers/TodoListController.scala
@@ -0,0 +1,51 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.{Json, Writes}
+import scala.collection.mutable
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class TodoListController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  case class TodoListItem(id: Long, description: String, isItDone: Boolean)
+  implicit val todoListItemWrites: Writes[TodoListItem] = Json.writes[TodoListItem]
+
+  private val todoList = new mutable.ListBuffer[TodoListItem]()
+  todoList += TodoListItem(1, "test", true)
+  todoList += TodoListItem(2, "some other value", false)
+
+  def getAll(): Action[AnyContent] = Action {
+  if (todoList.isEmpty) {
+    NoContent
+  } else {
+    Ok(Json.toJson(todoList))
+  }
+}
+
+  def randomNumber = Action{
+    Ok(util.Random.nextInt(100).toString)
+  }
+
+  def getById(itemId: Long) = Action {
+    val foundItem = todoList.find(_.id == itemId)
+    foundItem match {
+      case Some(item) => Ok(Json.toJson(item))
+      case None => NotFound
+  }
+}
+
+
+  def deleteAllDone() = Action {
+    todoList.filterInPlace(_.isItDone == false)
+    Accepted
+  }
+
+  
+
+}
